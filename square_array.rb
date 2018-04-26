76bebbc7eb839c359b1a0c64f6c3bb811256041f
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-04-26 21:52:20 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -6,3 +6,13 @@ def square_array(array)
   end
   return new_array  # your code here
 end
+
+def square_array(array)
+  
+  array.each do |element|
+    new_array = []
+    new_array << element**2
+    new_array
+  end
+  return new_array  # your code here
+end
